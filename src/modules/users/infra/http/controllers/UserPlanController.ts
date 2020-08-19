86103fa93fcfff9c9d5530b21658cac3157cdfbf
Schlanger <leoschlanger@gmail.com>
2020-08-19 15:00:57 -0300

    fix

--- src/modules/users/infra/http/controllers/UserPlanController.ts
+++ src/modules/users/infra/http/controllers/UserPlanController.ts
@@ -4,13 +4,13 @@ import { classToClass } from 'class-transformer';
 
 import UpdateUserPlanService from '@modules/users/services/users/UpdateUserPlanService';
 
-export default class UserAvatarController {
+export default class UserPlanController {
   public async update(request: Request, response: Response): Promise<Response> {
     const { plan_id } = request.params;
 
-    const updateUserAvatar = container.resolve(UpdateUserPlanService);
+    const updateUserPlan = container.resolve(UpdateUserPlanService);
 
-    const user = await updateUserAvatar.execute({
+    const user = await updateUserPlan.execute({
       user_id: request.user.id,
       plan_id,
     });
